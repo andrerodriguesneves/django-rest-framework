407f8f600b7f7b5a04826ebd9712399cb86661b6
André Rodrigues <andre.rodriguesneves@gmail.com>
2022-05-26 17:15:47 -0300

    Atualização das views

--- cursos/views.py
+++ cursos/views.py
@@ -1,12 +1,37 @@
 from rest_framework import generics
+from rest_framework.generics import get_object_or_404
+
 from .models import Curso, Avaliacao
 from .serializers import CursoSerializer, AvaliacaoSerializer
 
 
-class CursoAPIView(generics.ListCreateAPIView):
+# Aqui nao precisa do identificador. Listas os cursos e Cria
+class CursosAPIView(generics.ListCreateAPIView):
     queryset = Curso.objects.all()
     serializer_class = CursoSerializer
 
-class AvaliacaoAPIView(generics.ListCreateAPIView):
+
+# Aqui precisa de Identificador.
+class CursoAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
+
+
+class AvaliacoesAPIView(generics.ListCreateAPIView):
     queryset = Avaliacao.objects.all()
     serializer_class = AvaliacaoSerializer
+
+    def get_queryset(self):
+        if self.kwargs.get('curso_pk'):
+            return self.queryset.filter(curso_id=self.kwargs.get('curso_pk'))
+        return self.queryset.all()
+
+
+class AvaliacaoAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Avaliacao.objects.all()
+    serializer_class = AvaliacaoSerializer
+
+    def get_object(self):
+        if self.kwargs.get('curso_pk'):
+            return get_object_or_404(self.get_queryset(), curso_id=self.kwargs.get('curso_pk'), pk=self.kwargs.get('avaliacao_pk'))
+        return get_object_or_404(self.get_queryset(), pk=self.kwargs.get('avaliacao_pk'))
